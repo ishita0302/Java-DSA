dc2bc32c5305be5ea83de49479186f829b0bac06
Ishita.Srivastava <isha.sri2002@gmail.com>
2023-12-25 17:13:38 +0530

    Create BitManipulation.java

---
+++ BitManipulation.java
@@ -0,0 +1,14 @@
+//import java.util.*;
+public class BitManipulation {
+    public static void main(String args[]){
+        int n=5;
+        int pos=2;
+        int bitMask=1<<pos;
+        if((bitMask & n)==0){
+            System.out.println("0");
+        }
+        else{
+            System.out.println("1");
+        }
+    }
+}
