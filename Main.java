11149ad17d26c3f4f59f6e30dee4717cd70c261f
Ishita.Srivastava <isha.sri2002@gmail.com>
2023-12-25 17:13:45 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        /*Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        int a = s.nextInt();
+        System.out.println(name);
+        System.out.println(a);*/
+
+        //int[] arr=new int[20];
+
+        //s.charAt(i)......for printing string character one by one.
+        //if(s1.compareTo(s2)==0)....0-equal, +ve-s1>s2, -ve-s1<s2
+        //s.substring(start index, end index)......end index not included
+        //StrigBuilder..........s.charAt(0), s.setCharAt(0,'a'), s.insert(0,'a'), s.delete(0,5(not included)), s.append('a'), s.length()
+        
+    }
+}
